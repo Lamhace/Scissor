521878acdb2f55f4bf8512867990348ff58500f8
Oladimeji <oladimejimatthew433@gmail.com>
2024-02-16 14:06:53 +0100

    ALMOST DONE!

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -53,7 +53,7 @@ export default function Header() {
           <img
             src={HeaderLogo}
             alt="Logo"
-            className=" xl:w-max lg:w-32 sm:w-24 xs:w-20 xs:absolute top-8 left-5 sm:static sm:mt-1 lg:mt-0 xl:mt-1 2xl:mt-3"
+            className=" xl:w-max lg:w-32 sm:w-24 xs:w-20 xs:absolute top-9 left-5 sm:static sm:mt-1 lg:mt-0 xl:mt-1 2xl:mt-3"
           />
         </Link>
       </div>
